8df93489d463f7a1ad8dcb0cc91a0b1ac35f6f02
lmitelman <lautaromitelman@gmail.com>
2025-05-17 16:33:04 -0300

    refactor(vuln): update import paths for use cases in vulns.module.ts

--- src/vulns/vulns.module.ts
+++ src/vulns/vulns.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VulnEntity } from './infrastructure/entities/VulnEntity';
 import { VulnController } from './infrastructure/controllers/VulnController';
-import { CreateVulnUseCase } from '@vulns/domain/usecases/CreateVulnUseCase';
-import { UpdateVulnUseCase } from '@vulns/domain/usecases/UpdateVulnUseCase';
-import { GetVulnUseCase } from '@vulns/domain/usecases/GetVulnUseCase';
-import { ListVulnsUseCase } from '@vulns/domain/usecases/ListVulnsUseCase';
-import { DeleteVulnUseCase } from '@vulns/domain/usecases/DeleteVulnUseCase';
 import ProviderLoader from 'src/config/ProviderLoader';
 import { MySQLVulnRepository } from './infrastructure/repository/MySQLVulnRepository';
+import { DeleteVulnUseCase } from './domain/usecases/DeleteVulnUseCase';
+import { GetVulnUseCase } from './domain/usecases/GetVulnUseCase';
+import { UpdateVulnUseCase } from './domain/usecases/UpdateVulnUsecase';
+import { CreateVulnUseCase } from './domain/usecases/CreateVulnUseCase';
+import { ListVulnsUseCase } from './domain/usecases/ListVulnsUseCase';
 
 const providers = ProviderLoader([
   VulnController,
