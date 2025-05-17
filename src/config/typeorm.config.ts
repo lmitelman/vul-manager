1a0d42c97b3639a95edd565ec185e412dacbcb3d
lmitelman <lautaromitelman@gmail.com>
2025-05-17 12:26:27 -0300

    config: enhance TypeORM configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,22 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
-export const getTypeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
-  type: 'mysql',
-  host: configService.get('MYSQLHOST'),
-  port: +configService.get('MYSQLPORT'),
-  username: configService.get('MYSQLUSER'),
-  password: configService.get('MYSQLPASSWORD'),
-  database: configService.get('MYSQLDATABASE'),
-  entities: [__dirname + '/../**/infrastructure/entities/**/*.ts'],
-  synchronize: configService.get('DB_SYNCHRONIZE', false),
-  logging: configService.get('DB_LOGGING', false),
-}); 
+export const getTypeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => {
+  console.log({
+    host: configService.get('MYSQLHOST'),
+    user: configService.get('MYSQLUSER'),
+    password: configService.get('MYSQLPASSWORD'),
+    database: configService.get('MYSQLDATABASE'),
+    port: configService.get('MYSQLPORT')
+  });
+
+  return {
+    type: 'mysql',
+    host: configService.get<string>('MYSQLHOST'),
+    port: parseInt(configService.get<string>('MYSQLPORT') || '3306', 10),
+    username: configService.get<string>('MYSQLUSER'),
+    password: configService.get<string>('MYSQLPASSWORD'),
+    database: configService.get<string>('MYSQLDATABASE'),
+    entities: [__dirname + '/../**/infrastructure/entities/**/*.ts']
+  };
+};
