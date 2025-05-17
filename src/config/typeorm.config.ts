9600b6c78b665260d34bcfa4a20a88d9290158d5
lmitelman <lautaromitelman@gmail.com>
2025-05-17 12:18:34 -0300

    config: update TypeORM configuration to use environment variables for MySQL connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,12 +3,12 @@ import { ConfigService } from '@nestjs/config';
 
 export const getTypeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: configService.get('DB_HOST', 'localhost'),
-  port: configService.get('DB_PORT', 3306),
-  username: configService.get('DB_USERNAME', 'root'),
-  password: configService.get('DB_PASSWORD', ''),
-  database: configService.get('DB_DATABASE', 'your_database_name'),
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  host: configService.get('MYSQLHOST'),
+  port: +configService.get('MYSQLPORT'),
+  username: configService.get('MYSQLUSER'),
+  password: configService.get('MYSQLPASSWORD'),
+  database: configService.get('MYSQLDATABASE'),
+  entities: [__dirname + '/../**/infrastructure/entities/**/*.ts'],
   synchronize: configService.get('DB_SYNCHRONIZE', false),
   logging: configService.get('DB_LOGGING', false),
 }); 
